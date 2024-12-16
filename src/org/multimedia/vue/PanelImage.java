fcabd238f744d07dee2edce69c8ea7f9a69c0a37
julia123456789037 <julia037.filia@gmail.com>
2024-12-16 15:26:42 +0100

    dabut ihm

---
+++ src/org/multimedia/vue/PanelImage.java
@@ -0,0 +1,28 @@
+package org.multimedia.vue;
+import org.multimedia.main.Controleur;
+import javax.swing.*;
+
+
+public class PanelImage extends JPanel
+{
+	Controleur ctrl;
+
+	public PanelImage (Controleur ctrl )
+	{
+		this.ctrl = ctrl;
+
+		/*-------------------------------*/
+		/* Création des composants       */
+		/*-------------------------------*/
+
+		/*-------------------------------*/
+		/* Positionnement des composants */
+		/*-------------------------------*/
+
+		/*-------------------------------*/
+		/* Activation des composants     */
+		/*-------------------------------*/
+	}
+
+
+}
