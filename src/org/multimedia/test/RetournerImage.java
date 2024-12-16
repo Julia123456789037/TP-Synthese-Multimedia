7dd73e317d78c9b725ee063c6f2b6706b0bffa81
Gabriel ROCHE <gabirocher@icloud.com>
2024-12-16 09:50:29 +0100

    Ajout image et test, fix structure classe ImageUtils

---
+++ src/org/multimedia/test/RetournerImage.java
@@ -0,0 +1,10 @@
+package org.multimedia.test;
+
+public class RetournerImage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/org/multimedia/util/PixelIterator.java
@@ -0,0 +1,12 @@
+package org.multimedia.util;
+
+/**
+ * Interface d'iteration à pixels.
+ * <br><br>
+ * <b>Date :</b> 14/12/2024
+ * @author Gabriel Roche
+ */
+public interface PixelIterator
+{
+	public void applyFilter(int x, int y);
+}
