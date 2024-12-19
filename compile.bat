919b98e1de9bcf70696360a5614fe03c9a293691
Gabriel ROCHE <gabirocher@icloud.com>
2024-12-19 15:54:14 +0100

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -1,3 +1,7 @@
 Get-ChildItem -Recurse -Filter *.java | Select-Object -ExpandProperty FullName > compile.list
 
 javac @compile.list -d bin/
+
+cd bin | jar cfe SwingPainter.jar org.multimedia.main.Controleur org/multimedia/main/Controleur.class *
+
+java -jar SwingPainter.jar
